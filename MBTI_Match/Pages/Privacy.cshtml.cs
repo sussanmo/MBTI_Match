23b7b3decc79e36b006bf1493d9559c412153e44
Suad Mohamed <t-smohamed@microsoft.com>
2024-06-12 10:58:07 -0700

    Basic logic for few activtivites/personality despite CRLF

---
+++ MBTI_Match/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MBTI_Match.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dataTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TImeStamp"] = dataTime;
+        }
+    }
+
+}
